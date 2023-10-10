0bd480bcdeec449a4fef74b013322ad08b6bfffb
Akib Mulla <akibmulla47@gmail.com>
2023-10-10 19:03:48 +0530

    Added social media links and fixed minor changes

--- src/App.js
+++ src/App.js
@@ -13,20 +13,41 @@ function App() {
     <>
       <div className='container'>
 
-        <section id='home'>
 
+        {/* social links */}
+        <div className='ribbon'>
 
-
-        <nav>
-
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>My Work</li>
-            <li>Contact</li>
+          <ul className='list'>
+            <li className='links'> <a href='https://github.com/ItzzAkib07' target='_blank'><i class="fa-brands fa-github"></i></a></li>
+            <li className='links'> <a href='https://www.linkedin.com/in/akib-mulla-931860194/' target='_blank'><i class="fa-brands fa-linkedin"></i></a></li>
+            <li className='links'> <a href='https://www.instagram.com/itzz._akib._____/' target='_blank'><i class="fa-brands fa-instagram"></i></a></li>
+            <li className='links'> <a href='https://www.facebook.com/akib.mulla.961' target='_blank'><i class="fa-brands fa-facebook"></i></a></li>
           </ul>
 
-        </nav>
+        </div>
+
+        {/* Home section */}
+        <section className='home' id='home'>
+
+          <nav>
+            <ul className='navbar'>
+              <SmoothScrollingLink to="home" >
+                <li className=' nav-link nav-link-ltr'>Home</li>
+              </SmoothScrollingLink>
+
+              <SmoothScrollingLink to="about" >
+                <li className=' nav-link nav-link-ltr'>About</li>
+              </SmoothScrollingLink>
+
+              <SmoothScrollingLink to="works" >
+                <li className=' nav-link nav-link-ltr'>My Work</li>
+              </SmoothScrollingLink>
+
+              <SmoothScrollingLink to="contact" >
+                <li className=' nav-link nav-link-ltr'>Contact</li>
+              </SmoothScrollingLink>
+            </ul>
+          </nav>
 
           <h1>Hi, I'm </h1>
 
@@ -40,7 +61,8 @@ function App() {
 
         </section>
 
-        <section className='About' id='about'>
+        {/* About section */}
+        <section className='about' id='about'>
 
           <div className='spline-viewer'>
             <Spline scene="https://prod.spline.design/aR8RJVAStugdKyXD/scene.splinecode" className='about-spline' />
@@ -49,26 +71,33 @@ function App() {
           <h1 className='about-title'>Hello</h1>
           <p className='sub-title'>There!!!</p>
 
+
+
+          <div className='info'>
+
+            <div className='avtaar'>
+              <img src={avtaar} />
+            </div>
+
+            <div className='myself'>
+              {/* <span style={{fontSize:'2rem', fontWeight:'bolder'}}>Who I'm</span> */}
+              <span>
+                I'm on a mission to transform imagination into innovation. As a web developer, I'm not just a code junkie; I'm a dream weaver, a digital architect, and a problem solver 🪄 ✨
+                <br />
+                My coding toolbox is filled with the latest technologies and frameworks, including HTML5, CSS3, JavaScript, React.js, Material UI and more. I'm always learning and evolving to stay on the cutting edge of web development.
+              </span>
+            </div>
+          </div>
+
         </section>
 
+        {/* Work section */}
+        <section className='works' id='works'>
+        </section>
 
-        <div className='info'>
-
-          <div className='avtaar'>
-            <img src={avtaar} />
-          </div>
-
-          <div className='myself'>
-            {/* <span style={{fontSize:'2rem', fontWeight:'bolder'}}>Who I'm</span> */}
-            <span>
-              I'm on a mission to transform imagination into innovation. As a web developer, I'm not just a code junkie; I'm a dream weaver, a digital architect, and a problem solver 🪄 ✨
-              <br />
-              My coding toolbox is filled with the latest technologies and frameworks, including HTML5, CSS3, JavaScript, React.js, Material UI and more. I'm always learning and evolving to stay on the cutting edge of web development.
-            </span>
-          </div>
-        </div>
-
-
+        {/* contact section */}
+        <section className='contact' id='contact'>
+        </section>
 
         {/* Scroll Top button */}
         <SmoothScrollingLink to="home" >
--- src/components/SmoothScrollingLink.js
+++ src/components/SmoothScrollingLink.js
@@ -6,8 +6,8 @@ const SmoothScrollingLink = ({ to, children }) => {
     <ScrollLink
       to={to}
       smooth={true}
-      duration={100}
-      offset={-80} // Change this offset based on your header height or sticky elements.
+      duration={1000}
+      offset={0} // Change this offset based on your header height or sticky elements.
       onClick={() => {
         // Handle any additional logic here if needed before scrolling.
         // document.getElementById('side-navbar').style.width = "0";
